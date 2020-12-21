5ebb93108e4773fbdc7d43ec837f9a36b4417b91
jwcnmr <64020175+jwcnmr@users.noreply.github.com>
2020-12-21 10:34:11 -0500

    Create sets.py

---
+++ Pythonpatterns/4. Arrays and lists/sets.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+# Examples of set uses
+
+fruit = {'apples', 'pears', 'lemons'}
+fruitPie = {'apples', 'pears'}
+
+# both are true if fruitPie
+# is a member of bruit
+print (fruitPie.issubset(fruit))
+print (fruitPie < fruit)
+
+# combine sets
+nuts = {'walnuts', 'pecans'}
+granola = fruit | nuts
+print(granola)
+
+data = set()    # create an empty set
+data.add (2.3)  # and add vales
+data.add (4.6)
+data.add(7.0)
+data.add(2.3)
+print (data)
