d19f7bfe6182df15fb3cea578a15a822559ebdc1
jwcnmr <64020175+jwcnmr@users.noreply.github.com>
2021-02-01 16:53:18 -0500

    Create Add2Radio.py

---
+++ newsletter/Add2Numbers/Add2Radio.py
@@ -0,0 +1,146 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
+# derived button
+# with simplified enable and disable methods
+
+class DButton(Button):
+    def __init__(self, master=None,  **kwargs):
+        super().__init__(master, width=6, command= self.comd, **kwargs)
+
+    def enable(self):
+        self['state'] = NORMAL
+
+    def disable(self):
+        self['state'] = DISABLED
+
+    def comd(self): pass
+
+class ChoiceButton(Radiobutton):
+    gvar = None  # will be set to an IntVar
+
+    def __init__(self, rt, med, label, index):
+        super().__init__(rt, text = label,
+                         variable =ChoiceButton.gvar,
+                         value=index, fg='blue',
+                         command = lambda: med.optClicked(self.text),
+                         font=(None, 15))
+
+        self.text = label
+        self.index = index
+        self.med = med
+
+class Mediator():
+    def __init__(self):
+         self.optval="+"
+
+         # dictionary of functions
+         self.funcs = {
+             '+': self.addfun,
+             '-': self.subfun,
+             '*': self.multfun,
+             '/': self.divfun
+         }
+
+    # save fields
+    def setFields(self, xentry, yentry):
+        self.xEntry = xentry
+        self.yEntry = yentry
+
+    # save the text of the operator
+    def optClicked(self, text):
+        self.optval = text
+
+    # save the labels
+    def setLabels(self, top, bottom):
+        self.topLabel = top
+        self.botLabel = bottom
+
+    def calcClicked(self):
+        try:
+            self.xval = float(self.xEntry.get())
+            self.yval = float(self.yEntry.get())
+            # get the function from the funcs dictionary
+            func = self.funcs.get(self.optval)
+
+            # do calculation and display result
+            self.botLabel.configure(text="res = " + str(func()))
+        except ValueError:
+            messagebox.showerror("Conversion error",
+                                 "Not numbers")
+
+    # the four functions the calc program can call
+    def addfun(self):
+        return self.xval + self.yval
+
+    def subfun(self):
+        return self.xval - self.yval
+
+    def multfun(self):
+        return self.xval * self.yval
+
+    def divfun(self):
+        if self.yval !=0:
+            return self.xval / self.yval
+        else:
+            return None
+
+# Button displays OK
+class CalcButton(DButton):
+    def __init__(self, master, med, **kwargs):
+        super().__init__(master, text="Calc", **kwargs)
+        self.med = med
+    def comd(self):
+        self.med.calcClicked()
+
+class BlueLabel(Label):
+    def __init__(self,root,  lbtext, **kwargs):
+        super().__init__(root,text=lbtext, justify=LEFT,  fg='blue' )
+
+class Builder():
+    def build(self):
+        root = tk.Tk()
+        topLabel = BlueLabel(root, "Enter 2 values:")
+        topLabel.grid(row=0, column=0, columnspan=4)
+
+        # create entry field
+        xlabel = BlueLabel(root, "Num1:").grid(row=1, column=0)
+        xEntry = Entry(root)
+        xEntry.grid(row=1, column=1, columnspan=3, padx=10)  # and put it into the window
+
+        ylabel = BlueLabel(root, "Num2:").grid(row=2, column=0)
+        yEntry = Entry(root)
+        yEntry.grid(row=2, column=1, columnspan=3,padx=10, pady=10)  # and put it into the window
+
+        med = Mediator()
+        ChoiceButton.gvar = IntVar()
+        ChoiceButton.gvar.set(0)
+
+        cbutton = ChoiceButton(root, med, '+', 0)
+        cbutton.grid(row=3, column=0)
+        cbutton = ChoiceButton(root, med, '-', 1)
+        cbutton.grid(row=3, column=1)
+        cbutton = ChoiceButton(root, med, '*', 2)
+        cbutton.grid(row=3, column=2)
+        cbutton = ChoiceButton(root, med, '/', 3)
+        cbutton.grid(row=3, column=3)
+
+        # OK button calls getName when clicked
+        self.addButton = CalcButton(root, med)
+        self.addButton.grid(row=4, column=1, columnspan=2)
+
+ # This is the label whose text changes
+        botLabel = BlueLabel(root, 'Sum')
+        botLabel.grid(row=5, column=0, columnspan=4)
+        med.setLabels(topLabel, botLabel)
+        med.setFields(xEntry, yEntry)
+        mainloop()
+
+def main():
+    bld = Builder()
+    bld.build()
+
+# starts here
+if __name__ == '__main__':
+    main()
