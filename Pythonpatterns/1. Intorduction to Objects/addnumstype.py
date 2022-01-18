9f43915e13bfc6984a8dcc51a9840d70c3fea26c
jwcnmr <64020175+jwcnmr@users.noreply.github.com>
2022-01-18 14:10:12 -0500

    Corrected addNumsTupe
    Python's polymorphism is more limited than the book suggests. This revised version works, thoug.

--- Pythonpatterns/1. Intorduction to Objects/addnumstype.py
+++ Pythonpatterns/1. Intorduction to Objects/addnumstype.py
@@ -1,10 +1,23 @@
+""" This is a revised version of this class.
+Actually Python does not allow multiple methods with the same name
+since it uses a one-pass compiler. So the intended polymorphism doesn't actually work.
+
+This example uses the type function to dispatch addNums(f,f) to addNumsF
+"""
+
 class Summer():
-    def addNums(self, x: float, y: float) ->float:
+    def addNumsf(self, x: float, y: float) ->float:
+        #print('f1')
         return x + y
 
     def addNums(self, f: float, s: str)->float:
-        fsum = f + float(s)
-        return fsum
+        #print(type(s))
+        if type(s) is float:
+            return self.addNumsf(f,s)
+        else:
+            fsum = f + float(s)
+            #print('f2')
+            return fsum
 
 #----------------------------------
 def main():
@@ -12,8 +25,8 @@ def main():
     print(sumr.addNums(12.0, 2.3))
     print(sumr.addNums(22.3, "13.5"))
     # This will fail
-    val = sumr.addNums('22.3', '13.5')
-    print(val)
+   # val = sumr.addNums('22.3', '13.5')
+    #print(val)
 
 ###  Here we go  ####
 if __name__== "__main__":
