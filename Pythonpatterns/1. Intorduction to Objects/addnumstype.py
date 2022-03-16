643cfd27631f4489cfd2efad51c08f7edb597c53
jwcnmr <64020175+jwcnmr@users.noreply.github.com>
2022-03-16 10:32:11 -0400

    Update addnumstype.py
    Added the multimethod library to support polymorphic calls to addNums

--- Pythonpatterns/1. Intorduction to Objects/addnumstype.py
+++ Pythonpatterns/1. Intorduction to Objects/addnumstype.py
@@ -1,32 +1,31 @@
 """ This is a revised version of this class.
 Actually Python does not allow multiple methods with the same name
-since it uses a one-pass compiler. So the intended polymorphism doesn't actually work.
-
-This example uses the type function to dispatch addNums(f,f) to addNumsF
+since it uses a one-pass compiler. So the intended polymorphism doesn't actually work
+without the use of the multimethod library as shown below.
 """
+from multimethod import multimeta
 
-class Summer():
-    def addNumsf(self, x: float, y: float) ->float:
-        #print('f1')
+class Summer(metaclass=multimeta):
+    def addNums(self, x: float, y: float) ->float:
+        print("f,f")
         return x + y
 
     def addNums(self, f: float, s: str)->float:
-        #print(type(s))
-        if type(s) is float:
-            return self.addNumsf(f,s)
-        else:
-            fsum = f + float(s)
-            #print('f2')
-            return fsum
+        print('f,s')
+        fsum = f + float(s)
+        return fsum
 
+    def addNums(self, s1: str, s2: str)->float:
+        print('s,s')
+        return float(s1) + float(s2)
 #----------------------------------
 def main():
     sumr = Summer()
     print(sumr.addNums(12.0, 2.3))
     print(sumr.addNums(22.3, "13.5"))
-    # This will fail
-   # val = sumr.addNums('22.3', '13.5')
-    #print(val)
+    # This will fail, but this version could be added
+    val = sumr.addNums('22.5', '13.5')
+    print(val)
 
 ###  Here we go  ####
 if __name__== "__main__":
