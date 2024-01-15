85b6045aa57e41a31f81025fb8d8bb9e6d065af0
jwcnmr <64020175+jwcnmr@users.noreply.github.com>
2024-01-15 11:52:01 -0500

    Add files via upload

---
+++ 01. Intro to cpp/example1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Add up the amount of fruit you have
+int main() {
+    int apples = 5;
+    int oranges = 7;
+    int fruits = apples + oranges;
+
+    // print out the sum
+    cout << "Total fruit " << fruits << endl;
+    return 0;
+}
