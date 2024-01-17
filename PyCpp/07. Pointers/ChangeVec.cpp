043bbd88e9eabe6def7d4f4dbb8de099f8f609b4
jwcnmr <64020175+jwcnmr@users.noreply.github.com>
2024-01-17 12:21:57 -0500

    Update ChangeVec.cpp
    removed heads

--- PyCpp/07. Pointers/ChangeVec.cpp
+++ PyCpp/07. Pointers/ChangeVec.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // SimpleVec.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
@@ -29,55 +28,3 @@ int main(){
     printvec(v);  
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-=======
-// SimpleVec.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-#include <vector>
-using std::vector;
-using std::cout;
-using std::endl;
-
-//print out contents of vector
-void printvec(vector<int> z) {
-    for (auto k : z) {
-        cout << k << " ";
-    }
-    cout << endl;
-}
-void changeVec(vector<int>& px) {
-//void changeVec(vector<int>px) {
-    if (2 < px.size()) {        
-        px[2] = 123;       
-    }
-    printvec(px);
-}
-
-int main(){
-    vector <int> v = { 3,6,7,8,12 };
-    changeVec(v);
-    printvec(v);  
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
->>>>>>> 788375d648c18c4339961a2476115e1a0e78bd31
