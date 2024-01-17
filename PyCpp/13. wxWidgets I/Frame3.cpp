7005592d1ee9447354d0dae6429acba68188cf70
jwcnmr <64020175+jwcnmr@users.noreply.github.com>
2024-01-17 12:42:04 -0500

    Corrected doubling
    Doubling ad nheaders

--- PyCpp/13. wxWidgets I/Frame3.cpp
+++ PyCpp/13. wxWidgets I/Frame3.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // For compilers that support precompilation, includes "wx/wx.h".
 
 //Frame 3 changes wxApp to Builder and moves OnInit 
@@ -44,50 +44,3 @@ bool Builder::OnInit() {
 }
 
 
-=======
-// For compilers that support precompilation, includes "wx/wx.h".
-
-//Frame 3 changes wxApp to Builder and moves OnInit 
-/// to below the IMPLEMENT_APP main call
-// and introduces the first Sizer
-
-#include <wx/wxprec.h>
-
-#ifndef WX_PRECOMP
-#include <wx/wx.h>
-#include "wx/app.h"
-#include "wx/sizer.h"
-#endif
-
-class Builder : public wxApp {
-public:
-    bool OnInit(); 
-};
-
-wxIMPLEMENT_APP(Builder);
-
-bool Builder::OnInit() {
-    wxFrame* frame = new wxFrame(NULL, wxID_ANY, "Hello World");
-    frame->SetSize(250, 200);
-    wxPanel* panel = new wxPanel(frame);
-    
-    //create the Box sizer
-    wxBoxSizer* vbox = new wxBoxSizer(wxVERTICAL);
-    panel->SetSizer(vbox);
-    
-    //create the text label
-    wxStaticText* tx = new wxStaticText(panel, wxID_ANY,
-        wxString("Greetings"));
-    vbox->AddSpacer(50);
-    vbox->Add(tx);    //and add it to the box
-    /* vbox->Add(tx, 0,    //not stretchable
-        wxALIGN_CENTER, //alignment
-        10);            //border width
-        */
-    
-    frame->Show(true);
-    return true;
-}
-
-
->>>>>>> 788375d648c18c4339961a2476115e1a0e78bd31
--- PyCpp/13. wxWidgets I/wxHello.py
+++ PyCpp/13. wxWidgets I/wxHello.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # Import the wxPython package.
 import wx
 
@@ -8,15 +8,5 @@ frm.SetInitialSize((250, 200))  #et the size
 frm.Show()  # Show it.
 
 # Start the event loop.
-=======
-# Import the wxPython package.
-import wx
 
-app = wx.App()  # Create an application object.
-frm = wx.Frame(None, title="Hello World") # Then a frame.
-frm.SetInitialSize((250, 200))  #et the size
-frm.Show()  # Show it.
-
-# Start the event loop.
->>>>>>> 788375d648c18c4339961a2476115e1a0e78bd31
 app.MainLoop()
